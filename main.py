a5ec4d9c58427e62673ba976eb1ee5f5e349920f
DeDxYk594 <48350334+DeDxYk594@users.noreply.github.com>
2019-11-02 19:03:40 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,322 @@
+# -*-encoding:utf-8-*-
+from datetime import datetime
+
+import pytz
+from kivy.app import App
+from kivymd.theming import ThemeManager
+from kivymd.uix.card import MDCardPost
+from kivymd.uix.navigationdrawer import NavigationDrawerIconButton
+from kivymd.uix.navigationdrawer import NavigationLayout
+from kivymd.uix.picker import MDThemePicker
+from kivymd.uix.snackbar import Snackbar
+
+SCHEDULE = {0: ["Русский язык", "История", "География", "Биология", "Алгебра", "Физика"],
+            1: ["География", "Английский язык", "Информатика", "Геометрия", "Литература", "Физическая физкультура"],
+            2: ["Биология", "История", "Химия", "Алгебра", "Физическая физкультура", "Физика"],
+            3: ["Русский язык", "Алгебра", "Литература", "Физика", "ОБЖ", "Английский язык"],
+            4: ["История", "Английский язык", "Физическая физкультура", "Литература", "Обществознание"],
+            5: ["Химия", "Музыка", "Русский язык", "Литература", "Геометрия"]}
+
+
+# Vspomogatelnaiye function
+def _categorize(res):
+    ret = {}
+    for i in res:
+        ret[tuple(_district_cats(i))] = res[i]
+
+    ret0 = {}
+    keys = {}
+    for i in ret:
+        key = []
+        cat = [ret0]
+        for j in i:
+            key.append(j)
+            try:
+                cat.append(cat[-1][j])
+            except:
+                cat[-1][j] = {}
+                cat.append(cat[-1][j])
+        cat[-1]["__value__"] = ret[tuple(key)]
+
+    return ret0
+
+
+def _district_cats(res):
+    ret = [""]
+    mode = 0
+    for i in res:
+        if i in "()":
+            if i == "(" and not mode:
+                ret.append("")
+                mode += 1
+            elif i == ")" and mode:
+                mode -= 1
+        else:
+            ret[-1] += i
+    ret0 = [i.strip() for i in ret]
+
+    return ret0
+
+
+# Lets build a communistic PROject! (c)Gosha
+def communise(r):
+    tmp = repr(_categorize(r))
+    level = -1
+    maxlevel = -1
+    ret = []
+    tmp0 = []
+    now = []
+    for i in tmp:
+        if i == "{":
+            level += 1
+            if level > maxlevel:
+                maxlevel += 1
+            now.clear()
+        elif i in "\"'":
+            if "'" in tmp0:
+                tmp0.remove("'")
+                if now:
+                    if now[0] != "__value__:":
+                        ret[-1][1] += ":"
+                else:
+                    ret[-1][1] += ":"
+
+
+            else:
+                tmp0.append("'")
+                try:
+                    if ret[-1][1] == "__value__:":
+                        ret.remove(ret[-1])
+                except:
+                    pass
+                ret.append([level + 0, ""])
+        elif i == "}":
+            level -= 1
+            now.clear()
+        elif i == "," and not "'" in tmp0:
+            now.clear()
+        elif i == ":":
+            try:
+                now.append(ret[-1][1])
+            except:
+                pass
+
+        else:
+            if "'" in tmp0:
+                ret[-1][1] += i
+
+    for i in range(20):
+        try:
+
+            ret.remove([i, "__value__:"])
+        except:
+            pass
+    return ret, maxlevel
+
+
+# Карточка с предметом, используется для отображения списка текущих заданий
+class ItemCard(MDCardPost):
+    def __init__(self, **_):
+        super().__init__(**_)
+        self.swipe = True
+        self.path_to_avatar = "assets\pencil.png"
+        self.data_for_cancel={}
+
+    def callback(self, inst, value):
+        if value:
+            print(value)
+        else:
+            Snackbar(text="Удалено", button_text="Отмените!1", button_callback=self.btn_callback).show()
+            self.data_for_cancel=self.data
+            app.remove_from_data(self.header)
+
+    def btn_callback(self, *_):
+        app.cancel_delete(self.data_for_cancel)
+        Snackbar(text="Отменено!").show()
+
+    def set_data(self, data, header, nextday=False,addiction=None):
+
+        data_visual = communise(data)[0]
+        print(data_visual)
+        vis = ""
+        for i in data_visual:
+            vis+="   "*i[0]
+            vis+=i[1]
+            vis += "\n"
+        vis = vis[:-1]
+        print("vis={}".format(vis))
+
+        if not addiction:
+            addiction={False:"На потом",True:"На завтра"}[bool(nextday)]
+
+        self.data=data
+        self.header=header
+        self.nextday=nextday
+        self.addiction=addiction
+
+        self.text_post=vis
+        self.name_data="{}\n{}".format(self.header,self.addiction)
+
+
+
+
+# Кнопка в NavigationDrawerе, для удобства была переименована
+class NavButton(NavigationDrawerIconButton):
+    def __init__(self, *_, **__):
+        super().__init__(*_, **__)
+        app.nav_buttons.append(self)
+
+
+# Базовый класс, отвечает за расположение всего
+class MainLayout(NavigationLayout):
+    def __init__(self, *_, **__):
+        super().__init__(*_, **__)
+        app.main_widget = self
+
+
+class MainApp(App):
+    # Переменные - переводы
+    # Объявлены, чтобы можно было получить к ним доступ из kv файла
+
+    # Метки в NavigationDrawerе
+    customization = "Кастомизация"
+    algoritmus = "Алгоритмус"
+    tasks = "Список домашки"
+    raspisanie = "Умное расписание"
+    netochnost = "Что-то не так?"
+    oproge = "О приложении"
+    settings = "Настройки"
+
+    # Метки интерфейса
+    algoritmus_promotes = "Алгоритмус предлагает сделать:"
+    change_theme = "Настроить внешний вид"
+
+    # Подзаголовки NavigationDrawerа
+    main_subheader = "Основное"
+    settings_subheader = "Опции"
+
+    def __init__(self):
+        # Объявление переменных, отвечающих за работу КЛАССА
+        self.algoritmus_data = {}
+        self.algoritmus_header = ""
+
+        # Маин виджет потом будет задан из тела MainLayout
+        self.main_widget = None
+
+        # Тем мэнеджер, куда же без него!
+        self.theme_cls = ThemeManager()
+
+        # Список всех кнопок
+        self.nav_buttons = []
+
+        # today`s weekday
+        self.now = datetime.now()
+        tz = pytz.timezone("Etc/GMT+4")
+        tz.localize(self.now)
+
+        self.title = "9В - ДЗ"
+
+        # Данные домашки
+        # todo сделать импорт из ВК
+        # Дата временная
+        self.data = {'Информатика(обе группы)': 'Не задано; делали проверочную',
+                     'Английский язык (с Гошей)': 'WB p21 wordlist columns 2,3; WB p21-22 ex 16,17 ',
+                     'Английский язык (без Гоши)': 'Неизвестно',"Русский язык(письменно)":"Задание 1"}
+
+        # Инит в суперклассе, иначе кинет ошибку
+        super().__init__()
+
+        # ThemePicker - то самое окошко, где выбирают цвета темы
+        self.md_theme_picker = MDThemePicker()
+
+    def remove_from_data(self, name):
+        data = dict(self.data)
+        for i in self.data:
+            if i.startswith(name):
+                del data[i]
+
+        self.data = data
+        print(self.data)
+        self.refresh_data()
+
+    def refresh_data(self):
+        self.now = datetime.now()
+        tz = pytz.timezone("Etc/GMT+4")
+        tz.localize(self.now)
+        print(self.now.weekday())
+
+        hierarchy = []
+        weekday = self.now.weekday()
+        for i in range(6):
+            i += 0
+            i += weekday
+            if i == 6:
+                i = 0
+            elif i > 6:
+                i -= 6
+
+            for j in SCHEDULE[i]:
+                if j not in hierarchy:
+                    hierarchy.append(j)
+
+        self.algoritmus_data = {}
+        self.algoritmus_header=""
+        for i in hierarchy:
+            for j in self.data:
+                if j.startswith(i):
+                    self.algoritmus_data[j] = self.data[j]
+                    self.algoritmus_header=i
+            if self.algoritmus_data:
+                break
+
+        self.nextday=False
+        if weekday in (5,6):
+            weekday=-1
+        weekday+=1
+        if self.algoritmus_header in SCHEDULE[weekday]:
+            self.nextday=True
+
+
+
+        print(self.algoritmus_data)
+        if self.data:
+            self.main_widget.ids.algoritmuscard.set_data(self.algoritmus_data, self.algoritmus_header, self.nextday)
+        else:
+            self.main_widget.ids.algoritmuscard.set_data({},"Наверное, ты всё сделал",False,"База данных пуста :(")
+
+    def update_db(self):
+        pass
+
+    def show_screen(self, name):
+        # Ставит другой экран по вызову
+        self.main_widget.ids.scr_mngr.current = name
+
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        return
+
+    def set_title_toolbar(self, title):
+        self.main_widget.ids.toolbar.title = eval("self." + title)
+
+    def theme_picker_open(self):
+        self.md_theme_picker.open()
+        self.main_widget.ids.theme_label.text = self.get_theme_string()
+
+    def get_theme_string(self):
+        return "Фон: {}\nБазовый цвет: {}\nВторичный цвет: {}".format(self.theme_cls.theme_style,
+                                                                      self.theme_cls.primary_palette,
+                                                                      self.theme_cls.accent_palette)
+    def cancel_delete(self,data):
+        for i in data:
+            self.data[i]=data[i]
+        self.refresh_data()
+
+    def refresh_names_navdrawer(self):
+        for i in self.nav_buttons:
+            val = i.val
+            i.text = eval(val)
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
